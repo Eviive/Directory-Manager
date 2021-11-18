99832b94d07afd647d3e2b11dedf40fd579f3929
Eviive <albert.vaillon21@gmail.com>
2021-11-18 19:11:13 +0100

    Fonction Sauvegarde

--- Gestion-Annuaire.c
+++ Gestion-Annuaire.c
@@ -6,10 +6,44 @@
 
 typedef struct informations
 {
-	char prenom[20], nom[20], ville[40], code_postal[6], telephone[15], email[30], metier[20];
+	char prenom[30], nom[30], ville[50], code_postal[10], telephone[20], email[50], metier[30];
 } infos;
 
-void Affichage(infos *personne, int indice)
+void AttributionCSV(infos * personne, int cpt_virgule, int cpt_var, char caractere)
+{
+	switch(cpt_virgule)
+	{
+	case 0:
+		(*personne).prenom[cpt_var] = caractere;
+		break;
+
+	case 1:
+		(*personne).nom[cpt_var] = caractere;
+		break;
+	
+	case 2:
+		(*personne).ville[cpt_var] = caractere;
+		break;
+
+	case 3:
+		(*personne).code_postal[cpt_var] = caractere;
+		break;
+	
+	case 4:
+		(*personne).telephone[cpt_var] = caractere;
+		break;
+	
+	case 5:
+		(*personne).email[cpt_var] = caractere;
+		break;
+	
+	case 6:
+		(*personne).metier[cpt_var] = caractere;
+		break;
+	}
+}
+
+void AffichageRecherche(infos *personne, int indice)
 {
 	system("cls");
 	printf("Personne %d", indice);
@@ -80,23 +114,34 @@ void Affichage(infos *personne, int indice)
 	system("cls");
 }
 
-void Sauvegarde(infos  (*personne)[])
+void Sauvegarde(infos  (*personne)[], int cpt_ligne)
 {
-	FILE * save = fopen("sauvegarde.csv", "w");
-	if (save == NULL)
+	FILE * save_file = fopen("sauvegarde.csv", "w");
+	if (save_file == NULL)
 	{
 		printf("Echec creation du fichier de sauvegarde\n");
 		exit(EXIT_FAILURE);
 	}
+	for (int i = 0; i <= cpt_ligne; i++)
+	{
+		fprintf(save_file, "%s,", (*personne)[i].prenom);
+		fprintf(save_file, "%s,", (*personne)[i].nom);
+		fprintf(save_file, "%s,", (*personne)[i].ville);
+		fprintf(save_file, "%s,", (*personne)[i].code_postal);
+		fprintf(save_file, "%s,", (*personne)[i].telephone);
+		fprintf(save_file, "%s,", (*personne)[i].email);
+		fprintf(save_file, "%s\n", (*personne)[i].metier);
+	}
+	fclose(save_file);
 }
 
 int main()
 {
-	int cpt_ligne = 0, i = 0, cpt_virgule = 0, cpt_var = 0;
+	char ligne[300];
+	infos personne[6000];
+	int cpt_ligne = 0, cpt_char = 0, cpt_virgule = 0, cpt_var = 0;
 	int choix_menu;
 	int nb_recherche;
-	char ligne[200];
-	infos personne[6000];
 	/****************Lecture du fichier****************/
 	FILE * fichier = fopen(nom_fichier, "r");
 	if (fichier == NULL)
@@ -107,61 +152,33 @@ int main()
 	/****************Assignation de toutes les cellules à des variables****************/
 	while (fgets(ligne, sizeof(ligne), fichier) != NULL)
 	{
-		i = 0;
-		cpt_virgule = 0;
-		while (ligne[i] != '\n')
+		while (ligne[cpt_char] != '\0')
 		{
-			if (ligne[i] == ',')
+			if (ligne[cpt_char] == ',' || ligne[cpt_char] == '\n')
 			{
+				AttributionCSV(&personne[cpt_ligne], cpt_virgule, cpt_var, '\0');
 				cpt_virgule++;
 				cpt_var = 0;
 			}
 			else
 			{
-				switch(cpt_virgule)
-				{
-				case 0:
-					personne[cpt_ligne].prenom[cpt_var] = ligne[i];
-					break;
-
-				case 1:
-					personne[cpt_ligne].nom[cpt_var] = ligne[i];
-					break;
-				
-				case 2:
-					personne[cpt_ligne].ville[cpt_var] = ligne[i];
-					break;
-				
-				case 3:
-					personne[cpt_ligne].code_postal[cpt_var] = ligne[i];
-					break;
-				
-				case 4:
-					personne[cpt_ligne].telephone[cpt_var] = ligne[i];
-					break;
-				
-				case 5:
-					personne[cpt_ligne].email[cpt_var] = ligne[i];
-					break;
-				
-				case 6:
-					personne[cpt_ligne].metier[cpt_var] = ligne[i];
-					break;
-				}
+				AttributionCSV(&personne[cpt_ligne], cpt_virgule, cpt_var, ligne[cpt_char]);
 				cpt_var++;
 			}
-			i++;
+			cpt_char++;
 		}
+		cpt_char = 0;
+		cpt_virgule = 0;
 		cpt_ligne++;
 	}
 	fclose(fichier);
 	/****************Menu****************/
 	do
 	{
-		// system("cls");  // en commentaire pour pouvoir voir les erreurs
+		system("cls");  // en commentaire pour pouvoir voir les erreurs
 		printf("Bienvenue dans le menu du gestionnaire d'annuaire\n");
 		printf("	-Saisir 1 pour effectuer une recherche par indice\n");
-		printf("	-Saisir 2 pour effectuer une fonctionnalite que je n'ai pas encore code\n");
+		printf("	-Saisir 2 pour effectuer une sauvegarde dans un nouveau fichier\n");
 		printf("	-Saisir 3 pour quitter\n\n");
 		scanf("%d", &choix_menu);
 		fflush(stdin);
@@ -189,20 +206,18 @@ int main()
 					}
 					else if (nb_recherche != 0)
 					{
-						Affichage(&personne[nb_recherche-1], nb_recherche);
+						AffichageRecherche(&personne[nb_recherche-1], nb_recherche);
 					}
 				}
 				while (nb_recherche != 0);
 				break;
 			
-			/****************Fonctionnalité pas encore codé****************/
+			/****************Sauvegarde dans un nouveau fichier****************/
 			case 2:
 				system("cls");
 				printf("Creation du fichier de sauvegarde\n\n");
-
 				infos (*pt_info)[] = &personne;
-
-				Sauvegarde(pt_info);
+				Sauvegarde(pt_info, cpt_ligne - 1);
 				system("pause > nul | echo Appuyez sur une touche pour continuer...");
 				system("cls");
 				break;
