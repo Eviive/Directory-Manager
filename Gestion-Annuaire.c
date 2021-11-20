3b0a397bc3de4c6d1d1bcede7dca84e64cfd262e
Eviive <albert.vaillon21@gmail.com>
2021-11-20 04:17:13 +0100

    Fonction SaisiInfo

--- Gestion-Annuaire.c
+++ Gestion-Annuaire.c
@@ -8,7 +8,7 @@
 
 typedef struct informations
 {
-	char prenom[30], nom[30], ville[50], code_postal[20], telephone[20], email[50], metier[30];
+	char prenom[30], nom[30], ville[50], code_postal[6], telephone[15], email[50], metier[30];
 } infos;
 
 void AttributionCSV(infos * personne, int cpt_virgule, int cpt_var, char caractere);
@@ -19,6 +19,8 @@ void DonneeManquante(infos (*personne)[], int cpt_ligne);
 int DonneeEmpty(infos * personne);
 void EcritureFichier(FILE * fichier, infos * personne);
 void Ajout(infos (*personne)[], int * cpt_ligne);
+void SaisiInfo(char * value, int size);
+void Modif(infos *personne, int indice);
 
 void AttributionCSV(infos * personne, int cpt_virgule, int cpt_var, char caractere)
 {
@@ -161,48 +163,77 @@ int DonneeEmpty(infos * personne)
 	return 0;
 }
 
-void EcritureFichier(FILE * fichier, infos * personne)
+void EcritureFichier(FILE * annuaire, infos * personne)
 {
-	fprintf(fichier, "%s,", (*personne).prenom);
-	fprintf(fichier, "%s,", (*personne).nom);
-	fprintf(fichier, "%s,", (*personne).ville);
-	fprintf(fichier, "%s,", (*personne).code_postal);
-	fprintf(fichier, "%s,", (*personne).telephone);
-	fprintf(fichier, "%s,", (*personne).email);
-	fprintf(fichier, "%s\n", (*personne).metier);
+	fprintf(annuaire, "%s,", (*personne).prenom);
+	fprintf(annuaire, "%s,", (*personne).nom);
+	fprintf(annuaire, "%s,", (*personne).ville);
+	fprintf(annuaire, "%s,", (*personne).code_postal);
+	fprintf(annuaire, "%s,", (*personne).telephone);
+	fprintf(annuaire, "%s,", (*personne).email);
+	fprintf(annuaire, "%s\n", (*personne).metier);
 }
 
 void Ajout(infos (*personne)[], int * cpt_ligne)
 {
-	printf("Saisir le prenom de la personne : ");
-	fgets((*personne)[(*cpt_ligne)].prenom, sizeof((*personne)[(*cpt_ligne)].prenom), stdin);
-	fflush(stdin);
-	(*personne)[(*cpt_ligne)].prenom[strlen((*personne)[(*cpt_ligne)].prenom) - 1] = '\0';
-	printf("Saisir le nom de la personne : ");
-	fgets((*personne)[(*cpt_ligne)].nom, sizeof((*personne)[(*cpt_ligne)].nom), stdin);
-	fflush(stdin);
-	(*personne)[(*cpt_ligne)].nom[strlen((*personne)[(*cpt_ligne)].nom) - 1] = '\0';
-	printf("Saisir la ville de la personne : ");
-	fgets((*personne)[(*cpt_ligne)].ville, sizeof((*personne)[(*cpt_ligne)].ville), stdin);
-	fflush(stdin);
-	(*personne)[(*cpt_ligne)].ville[strlen((*personne)[(*cpt_ligne)].ville) - 1] = '\0';
-	printf("Saisir le code postal de la personne : ");
-	fgets((*personne)[(*cpt_ligne)].code_postal, sizeof((*personne)[(*cpt_ligne)].code_postal), stdin);
-	fflush(stdin);
-	(*personne)[(*cpt_ligne)].code_postal[strlen((*personne)[(*cpt_ligne)].code_postal) - 1] = '\0';
-	printf("Saisir le numero de telephone de la personne : ");
-	fgets((*personne)[(*cpt_ligne)].telephone, sizeof((*personne)[(*cpt_ligne)].telephone), stdin);
-	fflush(stdin);
-	(*personne)[(*cpt_ligne)].telephone[strlen((*personne)[(*cpt_ligne)].telephone) - 1] = '\0';
-	printf("Saisir le email de la personne : ");
-	fgets((*personne)[(*cpt_ligne)].email, sizeof((*personne)[(*cpt_ligne)].email), stdin);
-	fflush(stdin);
-	(*personne)[(*cpt_ligne)].email[strlen((*personne)[(*cpt_ligne)].email) - 1] = '\0';
-	printf("Saisir le metier de la personne : ");
-	fgets((*personne)[(*cpt_ligne)].metier, sizeof((*personne)[(*cpt_ligne)].metier), stdin);
-	fflush(stdin);
-	(*personne)[(*cpt_ligne)].metier[strlen((*personne)[(*cpt_ligne)].metier) - 1] = '\0';
+	do
+	{
+		system("cls");
+
+		printf("Saisir le prenom de la personne : ");
+		SaisiInfo((*personne)[(*cpt_ligne)].prenom, sizeof((*personne)[(*cpt_ligne)].prenom));
+
+		printf("Saisir le nom de la personne : ");
+		SaisiInfo((*personne)[(*cpt_ligne)].nom, sizeof((*personne)[(*cpt_ligne)].nom));
+
+		printf("Saisir la ville de la personne : ");
+		SaisiInfo((*personne)[(*cpt_ligne)].ville, sizeof((*personne)[(*cpt_ligne)].ville));
+
+		printf("Saisir le code postal de la personne : ");
+		SaisiInfo((*personne)[(*cpt_ligne)].code_postal, sizeof((*personne)[(*cpt_ligne)].code_postal));
+
+		printf("Saisir le numero de telephone de la personne : ");
+		SaisiInfo((*personne)[(*cpt_ligne)].telephone, sizeof((*personne)[(*cpt_ligne)].telephone));
+
+		printf("Saisir le email de la personne : ");
+		SaisiInfo((*personne)[(*cpt_ligne)].email, sizeof((*personne)[(*cpt_ligne)].email));
+
+		printf("Saisir le metier de la personne : ");
+		SaisiInfo((*personne)[(*cpt_ligne)].metier, sizeof((*personne)[(*cpt_ligne)].metier));
+	}
+	while (strlen((*personne)[(*cpt_ligne)].prenom) == 0
+		&& strlen((*personne)[(*cpt_ligne)].nom) == 0
+		&& strlen((*personne)[(*cpt_ligne)].ville) == 0
+		&& strlen((*personne)[(*cpt_ligne)].code_postal) == 0
+		&& strlen((*personne)[(*cpt_ligne)].telephone) == 0
+		&& strlen((*personne)[(*cpt_ligne)].email) == 0
+		&& strlen((*personne)[(*cpt_ligne)].metier) == 0
+	);
+	// Faire les différents cas
+	// Enlever les parenthèses autour des pointeurs ?
+	
 	(*cpt_ligne)++;
+}
+
+void SaisiInfo(char * value, int size)
+{
+	fgets(value, size, stdin);
+	fflush(stdin);
+	for(int i = 0; i < strlen(value); i++ )
+	{
+    	if(value[i] == '\n')
+		{
+			value[i] = '\0';
+		}
+	}
+}
+
+void Modif(infos *personne, int indice)
+{
+	system("cls");
+	printf("Modif\n\n");
+	system("pause > nul | echo Appuyez sur une touche pour continuer...");
+	system("cls");
 }
 
 int main()
@@ -212,7 +243,7 @@ int main()
 	infos (*pt_infos)[] = &personne;
 	int cpt_ligne = 0, cpt_char = 0, cpt_virgule = 0, cpt_var = 0;
 	int choix_menu;
-	int nb_recherche;
+	int nb_recherche, nb_modif;
 	/****************Lecture du fichier****************/
 	FILE * annuaire_file = fopen(chemin_annuaire, "r");
 	if (annuaire_file == NULL)
@@ -252,10 +283,11 @@ int main()
 		printf("\t-Saisir 2 pour effectuer une sauvegarde dans un nouveau fichier\n");
 		printf("\t-Saisir 3 pour lister les personnes avec au moins une donnee manquante\n");
 		printf("\t-Saisir 4 pour ajouter une personne a l'annuaire\n");
-		printf("\t-Saisir 5 pour quitter\n\n");
+		printf("\t-Saisir 5 pour modifier une personne dans l'annuaire\n");
+		printf("\t-Saisir 6 pour quitter\n\n");
 		scanf("%d", &choix_menu);
 		fflush(stdin);
-		if (choix_menu >= 1 && choix_menu <= 5)
+		if (choix_menu >= 1 && choix_menu <= 6)
 		{
 			switch (choix_menu)
 			{
@@ -288,6 +320,7 @@ int main()
 				system("cls");
 				break;
 
+			/****************Client avec au moins une case vide****************/
 			case 3:
 				system("cls");
 				DonneeManquante(pt_infos, cpt_ligne - 1);
@@ -295,11 +328,34 @@ int main()
 				system("cls");
 				break;
 
+			/****************Ajout d'une personne****************/
 			case 4:
 				system("cls");
 				Ajout(pt_infos, &cpt_ligne);
+				printf("\n");
 				system("pause > nul | echo Appuyez sur une touche pour continuer...");
 				system("cls");
+				break;
+			
+			/****************Modification d'une personne****************/
+			case 5:
+				do
+				{
+					system("cls");
+					printf("Saisir le numero de la personne a modifier, il y a %d personnes rensignees (0 pour quitter) : ", cpt_ligne);
+					scanf("%d", &nb_modif);
+					fflush(stdin);
+					if (nb_modif > 0 && nb_modif <= cpt_ligne)
+					{
+						Modif(&personne[nb_modif-1], nb_modif);
+					}
+					else if (nb_modif != 0)
+					{
+						printf("\nErreur de saisie\n\n");
+						system("pause > nul | echo Appuyez sur une touche pour continuer...");
+					}
+				}
+				while (nb_modif != 0);
 				break;
 			}
 
@@ -310,6 +366,6 @@ int main()
 			system("pause > nul | echo Appuyez sur une touche pour continuer...");
 		}
 	}
-	while (choix_menu != 5);
+	while (choix_menu != 6);
 	return 0;
 }
