3b64ac0a4da7a20d3a9ac74168f551d38634f4f2
Eviive <albert.vaillon21@gmail.com>
2022-01-08 23:40:28 +0100

    Ajout des spécifications formelles

--- Fonction.c
+++ Fonction.c
@@ -2,25 +2,41 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
+/**
+:postcondition : détermine la commande a effectuer pour clear en fonction de l'OS
+*/
 void Clear()
 {
 	#ifdef _WIN32
 		system("cls");
-	#elif defined __unix__
+	#elif defined __unix__ || __APPLE__
 		system("clear");
 	#endif
 }
 
+/**
+:postcondition : détermine la commande a effectuer pour effectuer une pause en fonction de l'OS
+*/
 void Pause()
 {
 	#ifdef _WIN32
 		system("pause > nul | echo Appuyez sur une touche pour continuer...");
 	#elif defined __unix__
 		system("read -n1 -r -p 'Appuyez sur une touche pour continuer...'");
+	#elif defined __APPLE__
+		system("echo Appuyez sur une touche pour continuer...")
+		system("read touche")
 	#endif
 }
 
+/**
+:entrée personne : pointeur d'une structure de type infos
+:entrée value : int
+:pré-condition : value est entre 0 et 6
+:postcondition : renvoie le pointeur d'une des catégorie de la personne
+*/
 char * ChoixColonne(infos * personne, int value)
 {
 	switch (value)
@@ -51,7 +67,12 @@ char * ChoixColonne(infos * personne, int value)
 	}
 }
 
-char * NameColonne(int value)
+/**
+:entrée value : int
+:pré-condition : value est entre 0 et 6
+:postcondition : renvoie la chaine de caractères correspondant au nom de la catégorie
+*/
+char * NomColonne(int value)
 {
 	switch (value)
 	{
@@ -81,6 +102,13 @@ char * NameColonne(int value)
 	}
 }
 
+/**
+:entrée personne : tableau de structures de type infos
+:entrée indice_personne : tableau d'int
+:entrée cpt_ligne : pointeur d'int
+:pré-condition : cpt_ligne égal 0
+:postcondition : lit l'annuaire, stock toutes les informations dans le tableau de structures infos, cpt_ligne est égal au nombre de personne
+*/
 void Ouverture(infos personne[], int indice_personne[], int * cpt_ligne)
 {
 	char ligne[300]; //chemin_annuaire[100];
@@ -123,6 +151,12 @@ void Ouverture(infos personne[], int indice_personne[], int * cpt_ligne)
 	fclose(annuaire_file);
 }
 
+/**
+:entrée personne : structure de type infos
+:entrée indice : int
+:pré-condition : indice égal à l'indice de la personne dans l'annuaire
+:postcondition : affiche toutes les informations sur la personne
+*/
 void AffichageComplet(infos personne, int indice)
 {
 	int i, taille_aff;
@@ -164,6 +198,10 @@ void AffichageComplet(infos personne, int indice)
 	printf("%*.*s\n", -taille_aff, taille_aff, ChoixColonne(&personne, i));
 }
 
+/**
+:entrée value : pointeur de char
+:postcondition : affiche la valeur d'entrée si elle est non-nul, sinon affiche N/A
+*/
 void AffichageUnique(char * value)
 {
 	if (strlen(value) != 0 )
@@ -176,12 +214,18 @@ void AffichageUnique(char * value)
 	}
 }
 
+/**
+:entrée personne : pointeur de structure de type infos
+:entrée indice_personne : tableau d'int
+:entrée cpt_ligne : pointeur d'int
+:postcondition : ajoute une personne à la dernière ligne de l'annuaire
+*/
 void Ajout(infos * personne, int indice_personne[], int * cpt_ligne)
 {
 	Clear();
 	for (int i = 0; i <= 6; i++)
 	{
-		printf("Saisir %s : ", NameColonne(i));
+		printf("Saisir %s : ", NomColonne(i));
 		scanf("%s", ChoixColonne(personne, i));
 		fflush(stdin);
 	}
@@ -189,6 +233,14 @@ void Ajout(infos * personne, int indice_personne[], int * cpt_ligne)
 	(*cpt_ligne)++;
 }
 
+/**
+:entrée personne : tableau de structures de type infos
+:entrée indice_personne : tableau d'int
+:entrée cpt_ligne : pointeur d'int
+:entrée nb_suppression : int
+:pré-condition : nb_suppression égal à l'indice de la personne à supprimer
+:postcondition : décale toutes les personnes après l'indice passé en paramètre, permet d'écraser la personne à supprimer
+*/
 void Suppression(infos personne[], int indice_personne[], int * cpt_ligne, int nb_suppression)
 {
 	for (int i = nb_suppression; i < *(cpt_ligne) - 1; i++)
@@ -198,6 +250,11 @@ void Suppression(infos personne[], int indice_personne[], int * cpt_ligne, int n
 	(*cpt_ligne)--;
 }
 
+/**
+:entrée/sortie personne : pointeur de structure de type infos
+:pré-condition : personne est la structure à modifier
+:postcondition : modifie une des valeurs de la structure passé en entrée
+*/
 void Modif(infos * personne)
 {
 	char nb_col;
@@ -219,6 +276,12 @@ void Modif(infos * personne)
 	}
 }
 
+/**
+:entrée personne : tableau de structures de type infos
+:entrée indice_personne : tableau d'int
+:entrée cpt_ligne : int
+:postcondition : Affiche les données sur une personne si la fonction DonneeEmpty renvoie 1
+*/
 void DonneeManquante(infos personne[], int indice_personne[], int cpt_ligne)
 {
 	int indice = 0, cpt_clientvide = 0;
@@ -233,6 +296,10 @@ void DonneeManquante(infos personne[], int indice_personne[], int cpt_ligne)
 	printf("\nIl y a %d clients ayant au moins une case vide\n\n", cpt_clientvide);
 }
 
+/**
+:entrée personne : structure de type infos
+:postcondition : renvoie 1 si une des chaines de caractères de la structure est vide, sinon renvoie 0
+*/
 char DonneeEmpty(infos personne)
 {
 	for (int i = 0; i <= 6; i++)
@@ -245,6 +312,12 @@ char DonneeEmpty(infos personne)
 	return 0;
 }
 
+/**
+:entrée personne : tableau de structures de type infos
+:entrée indice_personne : tableau d'int
+:entrée cpt_ligne : int
+:postcondition : écrit dans un fichier de sauvegarde le contenu de chaque structure sur une ligne
+*/
 void Sauvegarde(infos personne[], int indice_personne[], int cpt_ligne)
 {
 	printf("Creation du fichier de sauvegarde\n\n");
@@ -256,7 +329,6 @@ void Sauvegarde(infos personne[], int indice_personne[], int cpt_ligne)
 	}
 	for (int i = 0; i <= cpt_ligne; i++)
 	{
-		// EcritureFichier(save_file, personne[indice_personne[i]]);
 		int j = 0;
 		for (j; j <= 5; j++)
 		{
@@ -268,8 +340,17 @@ void Sauvegarde(infos personne[], int indice_personne[], int cpt_ligne)
 	printf("Sauvegarde effectue (annuaire_sauvegarde.csv)\n\n");
 }
 
+/**
+:entrée personne : tableau de structures de type infos
+:entrée indice_personne : tableau d'int
+:entrée cpt_ligne : int
+:entrée nb_colonne : unsigned int
+:pré-condition : nb_colonne entre 0 et 6
+:postcondition : tri le tableau selon la colonne passé en paramètre
+*/
 void Tri(infos personne[], int indice_personne[], unsigned int nb_colonne, int cpt_ligne)
 {
+	clock_t start = clock();
 	int i = 1, j, petit;
 	while (i <= cpt_ligne - 1)
 	{
@@ -283,10 +364,22 @@ void Tri(infos personne[], int indice_personne[], unsigned int nb_colonne, int c
 		indice_personne[j + 1] = petit;
 		i++;
 	}
+	clock_t end = clock();
+	printf("Duree tri : %.lf ms\n\n",((double)(end - start) / CLOCKS_PER_SEC) * 1000);
 }
 
+/**
+:entrée personne : tableau de structures de type infos
+:entrée indice_personne : tableau d'int
+:entrée cpt_ligne : int
+:entrée str_prenom, str_nom, str_third : tableau de caractères
+:entrée nb_colonne : unsigned int
+:pré-condition : nb_colonne entre 0 et 6
+:postcondition : renvoie l'indice de la personne si elle est trouvée dans le tableau de structure, sinon renvoie -1
+*/
 int Recherche(infos personne[], int indice_personne[], int cpt_ligne, char str_prenom[], char str_nom[], char str_third[], unsigned int nb_colonne)
 {
+	clock_t start = clock(), end;
 	int i = 0;
 	while (i < cpt_ligne && strcasecmp(ChoixColonne(&personne[indice_personne[i]], 0), str_prenom) < 0)
 	{
@@ -297,17 +390,31 @@ int Recherche(infos personne[], int indice_personne[], int cpt_ligne, char str_p
 		if (strcasecmp(ChoixColonne(&personne[indice_personne[i]], 1), str_nom) == 0 && strcasecmp(ChoixColonne(&personne[indice_personne[i]], nb_colonne), str_third) == 0)
 		{
 			AffichageComplet(personne[indice_personne[i]], indice_personne[i]);
+			end = clock();
+			printf("Duree Recherche : %.lf ms\n\n",((double)(end - start) / CLOCKS_PER_SEC) * 1000);
 			return i;
 		}
 		i++;
 	}
+	end = clock();
+	printf("Duree Recherche : %.lf ms\n\n",((double)(end - start) / CLOCKS_PER_SEC) * 1000);
 	return -1;
 }
 
+/**
+:entrée personne : tableau de structures de type infos
+:entrée indice_personne : tableau d'int
+:entrée cpt_ligne : pointeur d'int
+:entrée nb_colonne : unsigned int
+:entrée str_filtre : tableau de caractères
+:pré-condition : nb_colonne entre 0 et 6
+:postcondition : effectue une recherche filtrée triée ("contient" et "début de chaine") et affiche les données des occurences trouvées
+*/
 void FiltreTrie(infos personne[], int indice_personne[], int cpt_ligne, unsigned int nb_colonne, char str_filtre[])
 {
+	clock_t start = clock();
 	int i = 0;
-	if (str_filtre[0] != '*' && str_filtre[strlen(str_filtre) - 1] != '*')// Séquentielle triée
+	if (str_filtre[0] != '*' && str_filtre[strlen(str_filtre) - 1] != '*') // Séquentielle triée
 	{
 		while (i < cpt_ligne && strcasecmp(ChoixColonne(&personne[indice_personne[i]], nb_colonne), str_filtre) < 0)
 		{
@@ -331,36 +438,54 @@ void FiltreTrie(infos personne[], int indice_personne[], int cpt_ligne, unsigned
 			i++;
 		}
 	}
+	clock_t end = clock();
+	printf("\nDuree filtre : %.lf ms\n\n",((double)(end - start) / CLOCKS_PER_SEC) * 1000);
 }
 
+/**
+:entrée personne : tableau de structures de type infos
+:entrée indice_personne : tableau d'int
+:entrée cpt_ligne : pointeur d'int
+:entrée nb_colonne : unsigned int
+:entrée str_filtre : tableau de caractères
+:pré-condition : nb_colonne entre 0 et 6
+:postcondition : effectue une recherche filtrée ("fin de chaine") et affiche les données des occurences trouvées
+*/
 void FinDeChaine(infos personne[], int indice_personne[], int cpt_ligne, unsigned int nb_colonne, char str_filtre[])
 {
+	clock_t start = clock();
 	int i = 0, j;
 	while (i < cpt_ligne)
 	{
 		int diff;
-		char elem[taille], elem_etu[taille] = "";
+		char elem[taille];
 		strcpy(elem, ChoixColonne(&personne[indice_personne[i]], nb_colonne));
 		diff = strlen(elem) - strlen(str_filtre);
 		if (diff >= 0)
 		{
-			j = 0;
-			for (int i = diff; i < strlen(elem); i++)
-			{
-				elem_etu[j] = elem[i];
-				j++;
-			}
-			if (strcasecmp(elem_etu, str_filtre) == 0)
+			if (strcasecmp(&elem[diff], str_filtre) == 0)
 			{
 				AffichageComplet(personne[indice_personne[i]], indice_personne[i]);
 			}
 		}
 		i++;
 	}
+	clock_t end = clock();
+	printf("\nDuree filtre : %.lf ms\n\n",((double)(end - start) / CLOCKS_PER_SEC) * 1000);
 }
 
+/**
+:entrée personne : tableau de structures de type infos
+:entrée indice_personne : tableau d'int
+:entrée cpt_ligne : pointeur d'int
+:entrée nb_colonne : unsigned int
+:entrée str_filtre : tableau de caractères
+:pré-condition : nb_colonne entre 0 et 6
+:postcondition : effectue une recherche filtrée ("sous-chaine") et affiche les données des occurences trouvées
+*/
 void SousChaine(infos personne[], int indice_personne[], int cpt_ligne, unsigned int nb_colonne, char str_filtre[])
 {
+	clock_t start = clock();
 	int i = 0;
 	while (i < cpt_ligne)
 	{
@@ -384,4 +509,6 @@ void SousChaine(infos personne[], int indice_personne[], int cpt_ligne, unsigned
 		}
 		i++;
 	}
+	clock_t end = clock();
+	printf("\nDuree filtre : %.lf ms\n\n",((double)(end - start) / CLOCKS_PER_SEC) * 1000);
 }
